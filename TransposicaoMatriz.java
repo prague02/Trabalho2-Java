9c711f14826231cab049b3cf65d4652dd4a39ff7
prague02 <laura.contact02@gmail.com>
2025-02-26 14:59:29 -0300

    Create TransposicaoMatriz.java

---
+++ TransposicaoMatriz.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class TransposicaoMatriz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] matriz = new int[3][3];
+        int[][] transposta = new int[3][3];
+
+        System.out.println("Digite os elementos da matriz 3x3:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+        // Calculando a matriz transposta
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                transposta[j][i] = matriz[i][j];
+            }
+        }
+
+        System.out.println("Matriz transposta:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(transposta[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
